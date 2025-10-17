8fdf7e7dc650c7fe2366e8adcaf00eecbd5c218d
Daniel Villafranqui <danedo20472@gmail.com>
2025-10-16 19:13:47 -0500

    App Layout & Navigation Updated

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Route::get('credits', function () { return view('credits'); })->name('credits');
+    Route::get('users', function () { return view('users'); })->name('users');
+    Route::get('customers', function () { return view('customers'); })->name('customers');
+    Route::get('reports', function () { return view('reports'); })->name('reports');
 });
+
 
 require __DIR__.'/auth.php';
