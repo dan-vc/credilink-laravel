7f5da6e8770411b029b7b86cb22c92edf8a27bf3
Daniel Villafranqui <danedo20472@gmail.com>
2025-10-16 19:48:52 -0500

    App Views added

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,3 +1,3 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center justify-center gap-2 px-5 py-3 bg-primary rounded-md font-semibold text-white hover:bg-secondary focus:bg-secondary active:bg-secondary']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center justify-center gap-2 px-5 py-3 rounded-lg font-semibold transition bg-primary text-white hover:bg-secondary hover:text-black']) }}>
     {{ $slot }}
 </button>
