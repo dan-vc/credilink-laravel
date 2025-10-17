55e59c7db6a7e05449e44253706a0da52e0d8208
Wizzar <juniorminano080@gmail.com>
2025-10-16 23:41:19 -0500

    Se agregaron las imagenes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,80 +11,82 @@
 
 <body>
     <div>
+        <img src="{{ Vite::asset('resources/images/img.png') }}" alt="Logo">
+    </div>
+
+    <div>
         <div>
-            <div>
-                <img src="" alt="">
-                <p>Créditos Rápidos</p>
-            </div>
-            <div>
-                <img src="" alt="">
-                <p>Pagos Rápidos</p>
-            </div>
-            <div>
-                <img src="" alt="">
-                <p>Préstamos sin intereses</p>
-            </div>
-            <div>
-                <img src="" alt="">
-                <p>Máxima Seguridad</p>
-            </div>
+            <img src="" alt="">
+            <p>Créditos Rápidos</p>
         </div>
-
         <div>
-            <div>
-                <div>
-                    <p>Sistema de Entidad Financiera</p>
-                </div>
-                <div>
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <!-- Email -->
-                        <div>
-                            <label for="email">Email</label>
-                            <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus
-                                autocomplete="username">
-                            @error('email')
-                                <div style="color: red;">{{ $message }}</div>
-                            @enderror
-                        </div>
-
-                        <!-- Password -->
-                        <div>
-                            <label for="password">Password</label>
-                            <input id="password" type="password" name="password" required
-                                autocomplete="current-password">
-                            @error('password')
-                                <div style="color: red;">{{ $message }}</div>
-                            @enderror
-                        </div>
-
-                        <!-- Submit -->
-                        <div>
-                            <div>
-                                <div>
-                                    <img src="" alt="">
-                                    <a href="">Iniciar Sesión Google</a>
-                                </div>
-                                <div>
-                                    <img src="" alt="">
-                                    <a href="">Iniciar Sesión GitHub</a>
-                                </div>
-                            </div>
-                            <button type="submit">Log in</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div>
-                <img src="" alt="">
-            </div>
+            <img src="" alt="">
+            <p>Pagos Rápidos</p>
         </div>
-
         <div>
-
+            <img src="" alt="">
+            <p>Préstamos sin intereses</p>
+        </div>
+        <div>
+            <img src="" alt="">
+            <p>Máxima Seguridad</p>
         </div>
     </div>
+
+    <div>
+        <div>
+            <div>
+                <p>Sistema de Entidad Financiera</p>
+            </div>
+            <div>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    <!-- Email -->
+                    <div>
+                        <label for="email">Email</label>
+                        <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus
+                            autocomplete="username">
+                        @error('email')
+                            <div style="color: red;">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <!-- Password -->
+                    <div>
+                        <label for="password">Password</label>
+                        <input id="password" type="password" name="password" required autocomplete="current-password">
+                        @error('password')
+                            <div style="color: red;">{{ $message }}</div>
+                        @enderror
+                    </div>
+
+                    <!-- Submit -->
+                    <div>
+                        <div>
+                            <div>
+                                <img src="" alt="">
+                                <a href="">Iniciar Sesión Google</a>
+                            </div>
+                            <div>
+                                <img src="" alt="">
+                                <a href="">Iniciar Sesión GitHub</a>
+                            </div>
+                        </div>
+                        <button type="submit">Log in</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div>
+            <img src="" alt="">
+        </div>
+    </div>
+
+    <div>
+
+    </div>
+
 
 </body>
 
