a88741c97b01fa48b139eff8d957a3cddac554d8
Daniel Villafranqui <danedo20472@gmail.com>
2025-10-16 19:48:52 -0500

    App Views added

---
+++ resources/views/components/search-input.blade.php
@@ -0,0 +1,1 @@
+<input {{ $attributes->merge(['class' => 'bg-[#eee] border-none focus:ring-primary text-sm rounded-md shadow-sm w-full max-w-72']) }}>
---
+++ resources/views/customers.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Clientes
+    </h2>
+</x-app-layout>
