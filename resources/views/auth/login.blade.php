56855bcc03f463a28589f474d87e0d39e4a4f239
Daniel Villafranqui <danedo20472@gmail.com>
2025-10-20 00:38:07 -0500

    fix/login styles & imgs

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,25 +11,25 @@
 
 <body>
     <div class="block-1">
-        <img src="{{ Vite::asset('public/img/login/logo.webp') }}" alt="Logo">
+        <img src="{{ asset('img/login/logo.webp') }}" alt="Logo">
     </div>
 
     <div class="block-2">
         <div class="card">
             <div>
-                <img src="{{ Vite::asset('public/img/login/creditos.webp') }}" alt="creditos">
+                <img src="{{ asset('img/login/creditos.webp') }}" alt="creditos">
                 <p>Créditos Rápidos</p>
             </div>
             <div>
-                <img src="{{ Vite::asset('public/img/login/pagos.webp') }}" alt="pagos">
+                <img src="{{ asset('img/login/pagos.webp') }}" alt="pagos">
                 <p>Pagos Rápidos</p>
             </div>
             <div>
-                <img src="{{ Vite::asset('public/img/login/prestamos.webp') }}" alt="prestamos">
+                <img src="{{ asset('img/login/prestamos.webp') }}" alt="prestamos">
                 <p>Préstamos sin intereses</p>
             </div>
             <div>
-                <img src="{{ Vite::asset('public/img/login/seguridad.webp') }}" alt="seguridad">
+                <img src="{{ asset('img/login/seguridad.webp') }}" alt="seguridad">
                 <p>Máxima Seguridad</p>
             </div>
         </div>
@@ -67,18 +67,18 @@
                         <div class="buttons">
                             <div class="buttons-social">
                                 <a href="{{ route('auth.google') }}" class="button-google">
-                                    <img src="{{ Vite::asset('public/img/login/google.webp') }}" alt="google">
+                                    <img src="{{ asset('img/login/google.webp') }}" alt="google">
                                     <p>Iniciar Sesión Google</p>
                                 </a>
                                 <a href="{{ route('auth.github') }}" class="button-github">
-                                    <img src="{{ Vite::asset('public/img/login/github.webp') }}" alt="github">
+                                    <img src="{{ asset('img/login/github.webp') }}" alt="github">
                                     <p>Iniciar Sesión GitHub</p>
                                 </a>
                             </div>
                             <div class="button-submit">
                                 <button type="submit">
                                     Entrar
-                                    <img src="{{ Vite::asset('public/img/login/entrar.webp') }}" alt="entrar">
+                                    <img src="{{ asset('img/login/entrar.webp') }}" alt="entrar">
 
                                 </button>
                             </div>
@@ -90,8 +90,8 @@
                 </div>
             </div>
             <div class="card-img">
-                <img src="{{ Vite::asset('public/img/login/imagen.webp') }}" alt="imagen">
-                <img src="{{ Vite::asset('public/img/login/logo 2.webp') }}" alt="logo 2">
+                <img src="{{ asset('img/login/imagen.webp') }}" alt="imagen">
+                <img src="{{ asset('img/login/logo 2.webp') }}" alt="logo 2">
             </div>
         </div>
 
